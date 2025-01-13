cbd535083c36caa0fe323f32c2023845e9d82090
Abdulazeez Salami <abdulazeezsalami19@gmail.com>
2025-01-13 14:20:35 +0100

    2501131420

--- public/components/footer.php
+++ public/components/footer.php
@@ -4,7 +4,7 @@
         </div>
     </div>
 
-    <!-- <script src="https://kit.fontawesome.com/ecdfbd10f7.js" crossorigin="anonymous"></script> -->
+    <script src="https://kit.fontawesome.com/ecdfbd10f7.js" crossorigin="anonymous"></script>
     <script src="src/script.js"></script>
 </body>
 </html>
